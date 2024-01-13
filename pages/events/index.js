763ff8f4ec2c36a5e255711eba551054d65b131e
GingerBeardx <ecryptographer@gmail.com>
2024-01-13 14:03:36 -0700

    Added EventsSearch component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,14 @@
 import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
 
 export default function AllEvents() {
   const events = getAllEvents();
 
   return (
-    <main>
+    <>
+      <EventsSearch />
       <EventList items={events} />
-    </main>
+    </>
   );
 }
