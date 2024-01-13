5cadd365d7efffb1ef175c6634719ab3871d1d2b
GingerBeardx <ecryptographer@gmail.com>
2024-01-13 11:14:31 -0700

    Event detail pages handled.

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,24 @@
+import { useRouter } from 'next/router';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+import { getEventById } from '../../dummy-data';
+
 export default function EventPage() {
+  const router = useRouter();
+  const event = getEventById(router.query.eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
   return (
-    <main>
-      <h1>My Event</h1>
-    </main>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics {...event} />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
   );
 }
