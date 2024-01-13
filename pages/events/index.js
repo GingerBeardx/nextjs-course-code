90a99e556d23f637cbffe9269730eb6343a4bb4c
GingerBeardx <ecryptographer@gmail.com>
2024-01-13 14:12:15 -0700

    Added search functionality

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,20 @@
+import { useRouter } from 'next/router';
+
 import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
 
 export default function AllEvents() {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    router.push(`/events/${year}/${month}`);
+  }
 
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
   );
