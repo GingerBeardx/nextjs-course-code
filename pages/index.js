938c5e767350b27ec02d6e397147c858c6008204
GingerBeardx <ecryptographer@gmail.com>
2024-01-14 14:57:38 -0700

    Refactor index.js and remove settings.json

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
 function HomePage(props) {
+  const { products } = props;
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products?.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
+}
+
+// executes before the component function runs and populates the data
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [
+        { id: 'p1', title: 'Product 1' },
+        { id: 'p2', title: 'Product 2' },
+        { id: 'p3', title: 'Product 3' },
+      ],
+    },
+  }; // will be passed to the page component as props
 }
 
 export default HomePage;
