0ea1265fac6de6515041e17b81ab57053f581e3a
GingerBeardx <ecryptographer@gmail.com>
2024-01-15 14:02:12 -0700

    Refactor dynamic page and fix link in index.js

--- pages/[uid].js
+++ pages/[uid].js
@@ -1,15 +1,16 @@
 export default function UserProfilePage(props) {
-  return <h1>{props.username}</h1>;
+  return <h1>{props.id}</h1>;
 }
 
 export async function getServerSideProps(context) {
   // req and res are Node.js specific that have a lot of information
   //about the request and response including the headers, cookies, etc.
   const { params, req, res } = context;
+  const userId = params.uid;
 
   return {
     props: {
-      username: 'Eric',
+      id: 'userid-' + userId,
       //No need for revalidate here, because this is server side rendering,
       //so the data is always fresh
     },
